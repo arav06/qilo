5608fe9efa6d1e884b078ed7ab0f5677e1c7e007
Arav <56185632+arav06@users.noreply.github.com>
2021-07-15 09:58:10 +0530

    Update Qilo.py

--- Qilo.py
+++ Qilo.py
@@ -33,140 +33,148 @@ except:
 username = gp.getuser()
 id = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=5))
 while True:
-    cmd = str(input(f"{username}@qilo-{id}$ "))
-    print("\n")
+    try:
+        cmd = str(input(f"{username}@qilo-{id}$ "))
 
-    # Checking if a command/module has been specified
-    if cmd == "":
-        print("Specify a command/module,type 'qhelp' to display the help menu\n")
-        continue
+        # Checking if a command/module has been specified
+        if cmd == "":
+            continue
 
-    # Pings the IPs in the list
-    elif cmd =="qping":
-        info2 = open(info,"r")
-        f = info2.read()
-        lines = f.split("\n")
-        print("\n")
-        for line in lines:
-            if line == "":
-                continue
+        # Pings the IPs in the list
+        elif cmd =="qping":
+            info2 = open(info,"r")
+            f = info2.read()
+            lines = f.split("\n")
+            print("\n")
+            for line in lines:
+                if line == "":
+                    continue
+                else:
+                    words = line.split()
+                    ip = words[0]
+
+                    print(f"[+]Results from {ip}")
+                    a = [str(os.system(f"ping {ip} -w 4"))]
+                    string = '' 
+                    for i in a:
+                        string += i   
+                    print(string)
+                print("\n")
+            continue
+
+        # Exits the program
+        elif cmd == "exit" or cmd == "quit":
+            print("[+]Bye :)\n")
+
+            break
+            exit()
+
+        # Clears the user's terminal 
+        elif cmd == "qclear":
+            name = os.name
+            if name == "nt":
+                os.system("cls")
+            elif name == "posix":
+                os.system("clear")
             else:
-                words = line.split()
-                ip = words[0]
-                
-                print(f"[+]Results for {ip}")
-                a = [str(os.system(f"ping {ip} -w 4"))]
-                string = '' 
-                for i in a:
-                    string += i   
-                print(string)
-        continue
+                print("\n[+]OS not supported :(")
+            continue
 
-    # Exits the program
-    elif cmd == "exit" or cmd == "quit":
-        print("[+]Bye :)\n")
+        # Prints the help menu 
+        elif cmd == "qhelp":
+            print("Welcome to Qilo SSH, an SSH client that lets you control multiple Windows/Linux Servers at once :) \n")
+            print("\nTypeany command/module in the prompt to control your servers \n")
+            print("Modules: \n")
+            print("1. Qping: Pings IPs specified in the list by typing 'qping' in the input \n")
+            print("2. Qclear: Clears your terminal by typing 'qclear' \n")
+            print("3. Qhelp: Displays the help menu by typing 'qhelp' \n")
+            print("4. QSys: Displays the System Information for all hosts by typing 'qsys' \n")
+            continue
 
-        break
-        exit()
+        # Prints OS information for all hosts
+        elif cmd == "qsys":
+            try:
+                info2 = open(info,"r")
+                f = info2.read()
+                lines = f.split("\n")
 
-    # Clears the user's terminal 
-    elif cmd == "qclear":
-        name = os.name
-        if name == "nt":
-            os.system("cls")
-        elif name == "posix":
-            os.system("clear")
+                for line in lines:
+                    if line == "":
+                        continue
+                    else:
+                        words = line.split()
+                        ip = words[0]
+                        port = words[1]
+                        user = words[2]
+                        passwd = words[3]
+                        ssh = paramiko.SSHClient()
+                        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                        ssh.connect(ip, port, user, passwd)
+                        command1 = "wmic os get Status, Caption, BuildNumber, OSArchitecture"
+                        command2 = "uname -a"
+                        stdin, stdout, stderr = ssh.exec_command(command1)
+                        stdin2, stdout2, stderr2 = ssh.exec_command(command2)
+                        output = stdout.readlines()
+                        output2 = stderr.readlines()
+                        output3 = stdout2.readlines()
+                        output4 = stderr2.readlines()
+                        string = '' 
+                        string2 = ''
+                        string3 = '' 
+                        string4 = ''
+                        for i in output:
+                            string += i 
+                        for j in output2:
+                            string2 += j  
+                        for k in output3:
+                            string3 += k
+                        for l in output4:
+                            string4 += l
+                        print("\n") 
+                        print(f"[+]Results from {ip}\n")
+                        print(string)
+                        print(string2)
+                        print(string3)
+                        print(string4)
+                        print("\n")
+            except:
+                print("\n[+]Could not connect :(\n")
+            continue
+
+         # Executing commands on the servers
         else:
-            print("OS not supported :(")
-        continue
-
-    # Prints the help menu 
-    elif cmd == "qhelp":
-        print("Welcome to Qilo SSH, an SSH client that lets you control multiple Windows/Linux Servers at once :) \n")
-        print("Developed by Arav Budhiraja(https://github.com/arav06) in 2021 \n")
-        print("\nTypeany command/module in the prompt to control your servers \n")
-        print("Modules: \n")
-        print("* Qping: Pings IPs specified in the list by typing 'qping' in the input \n")
-        print("* Qclear: Clears your terminal by typing 'qclear' \n")
-        print("* Qhelp: Displays the help menu by typing 'qhelp' \n")
-        print("* QSys: Displays the System Information for all hosts by typing 'qsys' \n")
-        continue
-    
-    # Prints OS information for all hosts
-    elif cmd == "qsys":
-        info2 = open(info,"r")
-        f = info2.read()
-        lines = f.split("\n")
-        
-        for line in lines:
-            if line == "":
-                continue
-            else:
-                words = line.split()
-                ip = words[0]
-                port = words[1]
-                user = words[2]
-                passwd = words[3]
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(ip, port, user, passwd)
-                command1 = "wmic os get Status, Caption, BuildNumber, OSArchitecture"
-                command2 = "uname -a"
-                stdin, stdout, stderr = ssh.exec_command(command1)
-                stdin2, stdout2, stderr2 = ssh.exec_command(command2)
-                output = stdout.readlines()
-                output2 = stderr.readlines()
-                output3 = stdout2.readlines()
-                output4 = stderr2.readlines()
-                string = '' 
-                string2 = ''
-                string3 = '' 
-                string4 = ''
-                for i in output:
-                    string += i 
-                for j in output2:
-                    string2 += j  
-                for k in output3:
-                    string3 += k
-                for l in output4:
-                    string4 += l
-                print("\n") 
-                print(f"[+]Results from {ip}\n")
-                print(string)
-                print(string2)
-                print(string3)
-                print(string4)
-                print("\n")
-        continue
-
-     # Executing commands on the servers
-    else:
-        info2 = open(info,"r")
-        f = info2.read()
-        lines = f.split("\n")
-        for line in lines:
-            if line == "":
-                continue
-            else:
-                words = line.split()
-                ip = words[0]
-                port = words[1]
-                user = words[2]
-                passwd = words[3]
-                ssh = paramiko.SSHClient()
-                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(ip, port, user, passwd)
-                stdin, stdout, stderr = ssh.exec_command(cmd)
-                output = stdout.readlines()
-                output2 = stderr.readlines()
-                string = '' 
-                string2 = '' 
-                for i in output:
-                    string += i   
-                for j in output2:
-                    string2 += j
-                print(f"[+]Results from {ip} \n")
-                print(string)
-                print(string2)
-                print("\n")
-        continue
+            try:
+                info2 = open(info,"r")
+                f = info2.read()
+                lines = f.split("\n")
+                for line in lines:
+                    if line == "":
+                        continue
+                    else:
+                        words = line.split()
+                        ip = words[0]
+                        port = words[1]
+                        user = words[2]
+                        passwd = words[3]
+                        ssh = paramiko.SSHClient()
+                        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                        ssh.connect(ip, port, user, passwd)
+                        stdin, stdout, stderr = ssh.exec_command(cmd)
+                        output = stdout.readlines()
+                        output2 = stderr.readlines()
+                        string = '' 
+                        string2 = '' 
+                        for i in output:
+                            string += i   
+                        for j in output2:
+                            string2 += j
+                        print(f"[+]Results from {ip} \n")
+                        print(string)
+                        print(string2)
+                        print("\n")
+            except:
+                print("\n[+]Could not connect :(\n")
+            continue
+    except KeyboardInterrupt:
+        print("\n")
+        pass
